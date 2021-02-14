5dfd54ad3d34450a9583350079c6bf007dcd7cf5
Jon Spencer <spencer.jonathan@gmail.com>
2021-02-14 16:55:12 +0000

    Fixed 80 and over bug

--- install.mysql.utf8.sql
+++ install.mysql.utf8.sql
@@ -1074,3 +1074,8 @@ CREATE TABLE `c1jr0_md_mail` (
 
 ALTER TABLE `c1jr0_md_member` ADD `notes` text DEFAULT NULL AFTER `country`;
 ALTER TABLE `c1jr0_md_member_history` ADD `notes` text DEFAULT NULL AFTER `country`;
+ALTER TABLE `c1jr0_md_member` CHANGE `insurance_group` `insurance_group` VARCHAR(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+update c1jr0_md_member 
+inner join c1jr0_users u on u.username = "Jon"
+set insurance_group = '80 and over', mod_date = current_timestamp(), mod_user_id = u.id
+where insurance_group = '80 and ove';
